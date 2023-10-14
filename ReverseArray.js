3fb84adb8098bfba51177f0f80796e82dc41fa69
Sriikanth123 <sksriikanthsk@gmail.com>
2023-10-14 21:19:29 +0530

    first commit

---
+++ ReverseArray.js
@@ -0,0 +1,7 @@
+function sortArrayDescending(arr) {
+    return arr.sort().reverse();
+}
+
+const inputArray = [5, 3, 8, 1, 9];
+const sortedArray = sortArrayDescending(inputArray);
+console.log(sortedArray);
